3ec3e85bfb8be7c070a040b33e7e310101dfa2ab
Igor Klein <savonarola1452@yandex.ru>
2023-11-01 22:55:11 +0200

    Добавить проверку на наличие полей name, about, avatar

--- controllers/users.js
+++ controllers/users.js
@@ -38,14 +38,15 @@ const getUserById = (req, res) => {
 const createUser = (req, res, next) => {
   const { name, about, avatar } = req.body;
 
+  if (!name || !about || !avatar) {
+    return res.status(400).send({
+      message: 'Поля name, about, avatar являются обязательными',
+    });
+  }
+
   if (name.length < 2 || name.length > 30 || about.length < 2 || about.length > 30) {
     return res.status(400).send({
       message: 'Имя пользователя должно быть от 2 до 30 символов',
-    });
-  }
-  if (about.length < 2 || about.length > 30) {
-    return res.status(400).send({
-      message: 'Поле о себе должно быть от 2 до 30 символов',
     });
   }
 
